6a9e5632dc2fb74820a1992714de1c79e157a502
Christian Schudt <christian.schudt@gmx.de>
2022-04-14 18:15:04 +0200

    Make the fractal image zoomable

--- ui/src/main/kotlin/fractals/FractalApp.kt
+++ ui/src/main/kotlin/fractals/FractalApp.kt
@@ -2,6 +2,7 @@ package fractals
 
 import javafx.application.Application
 import javafx.application.Application.launch
+import javafx.beans.InvalidationListener
 import javafx.embed.swing.SwingFXUtils
 import javafx.geometry.Insets
 import javafx.scene.Scene
@@ -23,37 +24,60 @@ fun main() {
     launch(FractalApp::class.java)
 }
 
+@OptIn(DelicateCoroutinesApi::class)
 class FractalApp : Application() {
-    
-    @OptIn(DelicateCoroutinesApi::class)
+    val canvas: OverlayCanvas = OverlayCanvas(400.0, 400.0)
+
+    var p1 = Complex(-1.5, 1.0)
+    var p2 = Complex(0.5, -1.0)
+
     override fun start(primaryStage: Stage) {
 
-        val p1 = Complex(-1.5, 1.0)
-        val p2 = Complex(0.5, -1.0)
-        val fractal = Fractal(Fractal.Function.MANDELBROT, 255, p1, p2)
-        val canvas = OverlayCanvas(400.0, 400.0)
+        drawFractal(p1, p2)
 
-        GlobalScope.launch(Dispatchers.JavaFx) {
-            fractal.applyToImage(canvas.width.toInt(), canvas.height.toInt())
-                    .buffer(canvas.height.toInt())
-                    .collect {
-                        canvas.graphicsContext2D.pixelWriter.setArgb(it.x, it.y, it.color)
-                    }
-        }
-
-        val button = Button("Save")
-        button.setOnAction {
+        val saveButton = Button("Save")
+        saveButton.setOnAction {
             Files.newOutputStream(Path.of("fractal.png")).use {
                 val image = WritableImage(canvas.width.toInt(), canvas.height.toInt())
                 canvas.snapshot(null, image)
                 ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", it)
             }
         }
-
-        val vBox = VBox(20.0, canvas, button)
+        val resetButton = Button("Reset")
+        resetButton.setOnAction {
+            p1 = Complex(-1.5, 1.0)
+            p2 = Complex(0.5, -1.0)
+            drawFractal(p1, p2)
+        }
+        canvas.rectangle2DProperty.addListener(InvalidationListener {
+            val rect2D = canvas.rectangle2DProperty.get()
+            val ratioRe1 = rect2D.minX / canvas.width
+            val ratioRe2 = (rect2D.minX + rect2D.width) / canvas.width
+            val ratioIm1 = (rect2D.minY + rect2D.height) / canvas.height
+            val ratioIm2 = rect2D.minY / canvas.height
+            val re1 = p1.re + (p2.re - p1.re) * ratioRe1
+            val im1 = p2.im + (p1.im - p2.im) * ratioIm1
+            val re2 = p1.re + (p2.re - p1.re) * ratioRe2
+            val im2 = p2.im + (p1.im - p2.im) * ratioIm2
+            p1 = Complex(re1, im1)
+            p2 = Complex(re2, im2)
+            drawFractal(p1, p2)
+        })
+        val vBox = VBox(20.0, canvas, saveButton, resetButton)
         vBox.padding = Insets(20.0, 20.0, 20.0, 20.0)
         primaryStage.scene = Scene(vBox)
         primaryStage.show()
         primaryStage.centerOnScreen()
+    }
+
+    private fun drawFractal(p1: Complex, p2: Complex) {
+        val fractal = Fractal(Fractal.Function.MANDELBROT, 255, p1, p2)
+        GlobalScope.launch(Dispatchers.JavaFx) {
+            fractal.applyToImage(canvas.width.toInt(), canvas.height.toInt())
+                .buffer(canvas.height.toInt())
+                .collect {
+                    canvas.graphicsContext2D.pixelWriter.setArgb(it.x, it.y, it.color)
+                }
+        }
     }
 }
--- ui/src/main/kotlin/fractals/OverlayCanvas.kt
+++ ui/src/main/kotlin/fractals/OverlayCanvas.kt
@@ -1,5 +1,8 @@
 package fractals
 
+import javafx.beans.property.ReadOnlyObjectProperty
+import javafx.beans.property.ReadOnlyObjectWrapper
+import javafx.geometry.Rectangle2D
 import javafx.scene.canvas.Canvas
 import javafx.scene.canvas.GraphicsContext
 import javafx.scene.layout.StackPane
@@ -8,11 +11,19 @@ import kotlin.math.abs
 
 class OverlayCanvas(width: Double, height: Double) : StackPane() {
 
+    private val rectangle2D: ReadOnlyObjectWrapper<Rectangle2D>
+
     private val canvas: Canvas
+
+    private var tempRect:Rectangle2D = Rectangle2D(0.0, 0.0, 0.0, 0.0)
 
     val graphicsContext2D: GraphicsContext get() = canvas.graphicsContext2D
 
+    val rectangle2DProperty: ReadOnlyObjectProperty<Rectangle2D> get() = rectangle2D.readOnlyProperty
+
+
     init {
+        rectangle2D = ReadOnlyObjectWrapper(Rectangle2D(0.0, 0.0, width, height))
         canvas = Canvas(width, height)
         val overlay = Canvas()
         overlay.widthProperty().bind(canvas.widthProperty())
@@ -28,14 +39,26 @@ class OverlayCanvas(width: Double, height: Double) : StackPane() {
 
         setOnMouseReleased {
             overlay.graphicsContext2D.clearRect(0.0, 0.0, overlay.width, overlay.height)
+            rectangle2D.set(tempRect)
         }
 
         setOnMouseDragged {
             overlay.graphicsContext2D.clearRect(0.0, 0.0, overlay.width, overlay.height)
-            val x = if (it.x > startX) startX else it.x
-            val y = if (it.y > startY) startY else it.y
 
-            overlay.graphicsContext2D.fillRect(x, y, abs(it.x - startX), abs(it.y - startY))
+            var w = abs(it.x - startX)
+            var h = abs(it.y - startY)
+            val ratioCanvas = width / height
+            val ratioOverlay = w / h
+            val f = ratioOverlay / ratioCanvas
+            if (f < 1) {
+                h *= f
+            } else {
+                w /= f
+            }
+            val x = if (it.x > startX) startX else startX - w
+            val y = if (it.y > startY) startY else startY - h
+            tempRect = Rectangle2D(x, y, w, h)
+            overlay.graphicsContext2D.fillRect(x, y, w, h)
         }
 
         overlay.isMouseTransparent = true
